e42c4e6a7b3ff35da260ab4b7ff95332e19a02e1
tmhuyett <toddhuyett@gmail.com>
2020-12-04 22:38:53 -0500

    'sdfsdf'

--- functions/get-protected-content.js
+++ functions/get-protected-content.js
@@ -1,50 +1,34 @@
-const IMAGES = 
-
-[{
-  src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-  thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-  thumbnailWidth: 320,
-  thumbnailHeight: 174,
-  isSelected: true,
-  caption: "After Rain (Jeshu John - designerspics.com)"
-},
-{
-  src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-  thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-  thumbnailWidth: 320,
-  thumbnailHeight: 212,
-  tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
-  caption: "Boats (Jeshu John - designerspics.com)"
-},
-
-{
-  src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-  thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-  thumbnailWidth: 320,
-  thumbnailHeight: 212
-}];
-
 const content = {
   free: {
-  
-    source: 
-      IMAGES.src,
-      src:
-      'https://wwlibrary.org/contents/videos_screenshots/0/296/preview.jpg',
+    src:
+      'https://images.unsplash.com/photo-1550159930-40066082a4fc?auto=format&fit=crop&w=600&h=600&q=80',
+    alt: 'corgi in the park with a sunset in the background',
+    credit: 'Jacob Van Blarcom',
+    creditLink: 'https://unsplash.com/photos/lkzjENdWgd8',
+    message: 'To view this content, you need to create an account!',
+    allowedRoles: ['free', 'pro', 'premium'],
+  },
+  pro: {
+    src:
+      'https://images.unsplash.com/photo-1519098901909-b1553a1190af?auto=format&fit=crop&w=600&h=600&q=80',
     alt: 'close-up of a corgi with its tongue hanging out',
     credit: 'Florencia Potter',
     creditLink: 'https://unsplash.com/photos/yxmNWxi3wCo',
     message:
       'This is protected content! It’s only available if you have a pro plan or higher.',
-    allowedRoles: ['free','pro', 'premium'],
-  }
-
-  
-
-  
-
-}
-
+    allowedRoles: ['pro', 'premium'],
+  },
+  premium: {
+    src:
+      'https://images.unsplash.com/photo-1546975490-e8b92a360b24?auto=format&fit=crop&w=600&h=600&q=80',
+    alt: 'corgi in a tent with string lights in the foreground',
+    credit: 'Cole Keister',
+    creditLink: 'https://unsplash.com/photos/cX-KEISwDIw',
+    message:
+      'This is protected content! It’s only available if you have the premium plan.',
+    allowedRoles: ['premium'],
+  },
+};
 
 exports.handler = async (event, context) => {
   const { type } = JSON.parse(event.body);

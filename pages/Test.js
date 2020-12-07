e706425a78ca400d67e8480ceedc348b5e79fc40
tmhuyett <toddhuyett@gmail.com>
2020-12-06 20:20:44 -0500

    'sdsdfsdfsdf'

--- pages/Test.js
+++ pages/Test.js
@@ -158,7 +158,7 @@ src="https://identity.netlify.com/v1/netlify-identity-widget.js">
 
 <template id="content">
 <figure className="content-display">
-  <Gallery />
+  
 <img />
 <img id="img2" />
 <figcaption>
